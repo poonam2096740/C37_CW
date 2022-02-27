19e223ef2acd26fe32cf6c5a03d5567a54ccac7f
poonam2096740 <85899531+poonam2096740@users.noreply.github.com>
2022-02-28 02:12:53 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,43 @@
+class Game {
+  constructor() {}
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+  }
+
+  play() {
+    this.handleElements();
+
+    Player.getPlayersInfo();
+
+    if (allPlayers !== undefined) {
+      image(track, 0, -height * 5, width, height * 6);
+
+      drawSprites();
+    }
+  }
+}
